5fc014a33b2c8f25281333470867829321e713fa
Laupet <vblaurin@gmail.com>
2026-09-26 17:10:43 +0200

    comments to fast converter

--- fast_converter.py
+++ fast_converter.py
@@ -6,10 +6,12 @@ class GeometryNodeSerializer:
     def __init__(self, element):
         self.e = element
 
-    def isVector(self, slot):
-        return isinstance(slot, bpy.types.bpy_prop_array) or isinstance(slot, mathutils.Euler) or isinstance(slot, mathutils.Color) or isinstance(slot, mathutils.Vector)
+    # Returns true if the value is of a vector type
+    def isVector(self, value):
+        return isinstance(value, bpy.types.bpy_prop_array) or isinstance(value, mathutils.Euler) or isinstance(value, mathutils.Color) or isinstance(value, mathutils.Vector)
 
-    def vec2string(self, vector, prefix=""):
+    # Converts a given vector to multiple Value entries
+    def handle_vector(self, vector, prefix=""):
         len = vector.__len__()
         for i, v in enumerate(vector):
             ET.SubElement(
@@ -18,17 +20,21 @@ class GeometryNodeSerializer:
                 name = prefix + chr(ord('z')-(len-i-1)),
                 value = str(v)
             )
-        
-        if isinstance(vector, mathutils.Euler):
-            self.vec2string(vector.to_quaternion(), "q")
 
+        # If rotation, add quaternion angles too with q prefix
+        if isinstance(vector, mathutils.Euler):
+            self.handle_vector(vector.to_quaternion(), "q")
+
+    # Placeholder hash function, not guaranteed unique due to abs
     def hash_thing(self, node):
         return hex(abs(hash(node)))
 
+    # Handles serializing all slots of an element
     def handle_slots(self, slots):
         parent_elem = self.e
         
         for i, slot in enumerate(slots):
+            # Add Socket tag
             self.e = ET.SubElement(
                 parent_elem,
                 "Socket",
@@ -37,13 +43,16 @@ class GeometryNodeSerializer:
                 direction = "out" if slot.is_output else "in",
                 hash = self.hash_thing(slot)
             )
-            
+
+            # If default value exists, add it as a Value tag
             value = "None" 
             if "default_value" in dir(slot):
                 def_v = slot.default_value
                 if self.isVector(def_v):
-                    value = self.vec2string(def_v)
+                    # If it's a vector, parse differently
+                    value = self.handle_vector(def_v)
                 else:
+                    # Otherwise convert to string
                     value = str(def_v)
                     ET.SubElement(
                         self.e,
@@ -55,17 +64,19 @@ class GeometryNodeSerializer:
         self.e = parent_elem
             
 
+    # Serializes each node group, including the top level one
     def handle_node_group(self, node_group):
         olde = self.e
-        
+
+        # Add graph element
         self.e = ET.SubElement(
             self.e,
             "Graph",
             name = node_group.name
         )
-        
+
+        # Serialize Links
         self.links = {}
-        
         for link in node_group.links:
             self.links[str(self.hash_thing(link.from_socket))] = str(self.hash_thing(link.to_socket))
             self.links[str(self.hash_thing(link.to_socket))] = str(self.hash_thing(link.from_socket))
@@ -79,8 +90,10 @@ class GeometryNodeSerializer:
 
         
         parent_elem = self.e
-        
+
+        # Serialize nodes
         for node in node_group.nodes:
+            # Add node tag
             node_elem = ET.SubElement(
                 parent_elem,
                 "Node",
@@ -88,7 +101,8 @@ class GeometryNodeSerializer:
                 type = node.type
             )
             self.e = node_elem
-            
+
+            # Attributes to ignore
             irrelevant_attrs = [
                 "__doc__",
                 "__module__",
@@ -127,27 +141,31 @@ class GeometryNodeSerializer:
                 "name",
                 "hide"
             ]
+            # Attribute types to ignore
             irrelevant_attr_types = [
                 "bpy_func",
                 "bpy_prop_collection"
             ]
-            
+
             for a in dir(node):
-                if a not in irrelevant_attrs:
+                if a not in irrelevant_attrs: # filter irrelevant
                     v = getattr(node, a)
-                    if type(v).__name__ not in irrelevant_attr_types:
+                    if type(v).__name__ not in irrelevant_attr_types: # filter irrelevant types
                         parent_elem2 = self.e
+                        # Add attribute Tag
                         self.e = ET.SubElement(
                             self.e,
                             "Attribute",
                             type = type(v).__name__,
                             name = a,
                         )
+
+                        # Serialize value
                         if self.isVector(v):
-                            self.vec2string(v)
+                            self.handle_vector(v) # if vector
                         elif isinstance(v, bpy.types.GeometryNodeTree):
-                            self.handle_node_group(v)
-                        else:
+                            self.handle_node_group(v) # if subgraph recurse
+                        else: # if normal value
                             ET.SubElement(
                                 self.e,
                                 "Value",
@@ -155,11 +173,11 @@ class GeometryNodeSerializer:
                                 value = str(v)
                             )
                         self.e = parent_elem2
-                
+            
+            # Serialize the nodes slots
             self.handle_slots(node.inputs)
             self.handle_slots(node.outputs)
 
-            print()
         self.e = parent_elem
         self.e = olde
 
